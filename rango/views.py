70892338fe3a6559874dcc0c4952a2a62b13e4ba
Ignas Knizikevicius <2268993k@student.gla.ac.uk>
2018-02-08 17:22:32 +0000

    chapter8.3

--- rango/views.py
+++ rango/views.py
@@ -21,8 +21,12 @@ def index(request):
 
 
 
-def about (request):
-    return render(request, 'rango/about.html')
+def about(request):
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def add_page(request, category_name_slug):
     try:
